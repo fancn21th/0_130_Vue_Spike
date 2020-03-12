dfeca42088acafeae6c1fbc960c31625e7632cf8
范轶洁 <fanyjb@digitalchina.com>
2020-03-12 08:54:47 +0800

    add items

--- src/app/main.js
+++ src/app/main.js
@@ -3,6 +3,18 @@ var card = new Vue({
   data: {
     title: "Dinosaurs",
     content:
-      "<strong>Dinosaurs</strong> are a diverse group of animals of the clade <em>Dinosauria</em> that first appeared during the Triassic period."
+      "<strong>Dinosaurs</strong> are a diverse group of animals of the clade <em>Dinosauria</em> that first appeared during the Triassic period.",
+    items: [{ text: "foo" }, { text: "baz" }, { text: "bar" }]
+  },
+  methods: {
+    addItem: function() {
+      var input = document.getElementById("itemForm");
+      if (input.value !== "") {
+        this.items.push({
+          text: input.value
+        });
+        input.value = "";
+      }
+    }
   }
 });
